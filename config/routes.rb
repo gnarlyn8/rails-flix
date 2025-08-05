6633226611403c05931e2c26346a65b3cc6a0815
Nathen Bowes <nbowes17@gmail.com>
2025-08-05 10:08:50 -0700

    add edit functionality for movies

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update" 
 end
