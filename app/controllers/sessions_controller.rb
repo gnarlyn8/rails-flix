1a14b4694cd978aaebd7ca1ffaf6b5c78f0ab44e
Nathen Bowes <nbowes17@gmail.com>
2025-08-07 11:24:19 -0700

    add username lookup for authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email_or_username]) || 
+      User.find_by(username: params[:email_or_username])
+
+    if @user && @user.authenticate(params[:password])
+      redirect_to user_path(@user), notice: "Welcome back, #{@user.name}!"
+    else
+      flash.now[:alert] = "Email / Password did not match."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
