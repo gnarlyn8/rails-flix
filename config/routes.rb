5544b531575cd76ee355183d853e657df017e630
Nathen Bowes <nbowes17@gmail.com>
2025-08-07 11:02:13 -0700

    add sessions for user login

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+  get "login" => "sessions#new"
+  
   resources :users
   get "signup" => "users#new"
 end
