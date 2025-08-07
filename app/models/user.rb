67e0af657052d69eb909b3cd6b9fe357c841e998
Nathen Bowes <nbowes17@gmail.com>
2025-08-07 12:14:41 -0700

    address security vulnerability + linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, format: { with: /\S+@\S+/ },
-    uniqueness: { case_sensitive: false}
+  validates :email, format: { with: /\A\S+@\S+\z/ },
+    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
   validates :username, format: { with: /\A[A-Z0-9]+\z/i }
 
