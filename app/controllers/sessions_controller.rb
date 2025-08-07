01d78b1a1c6280d0fdccf6583935e40fe5e46017
Nathen Bowes <nbowes17@gmail.com>
2025-08-07 11:57:26 -0700

    add current_user functionality + sign out logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       User.find_by(username: params[:email_or_username])
 
     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Welcome back, #{@user.name}!"
     else
       flash.now[:alert] = "Email / Password did not match."
@@ -16,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to movies_url, status: :see_other, notice: "Successfully logged out."
   end
 end
