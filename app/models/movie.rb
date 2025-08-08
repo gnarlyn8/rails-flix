86d4924d5ec23e6c3b0ae42eafacc5085de1d7cd
Nathen Bowes <nbowes17@gmail.com>
2025-08-08 16:06:32 -0700

    linter

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
   has_many :critics, through: :reviews, source: :user
-  
+
   RATINGS = %w[G PG PG-13 R NC-17]
 
   validates :title, :released_on, :duration, presence: true
